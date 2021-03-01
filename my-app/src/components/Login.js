0c85aa34fce7527feec7bd686a12af46e79c9efb
DJcri <djcri123@gmail.com>
2021-03-01 10:28:58 -0800

    Initial commit

--- my-app/src/components/Login.js
+++ my-app/src/components/Login.js
@@ -1,6 +1,24 @@
 //Deavy
 //login form
 //sign-up link button
+import React from "react";
+import { Link } from "react-router-dom";
 
+const Login = (props) => {
+  return (
+    <div className="login-container">
+      <form>
+        <input type="text" placeholder="Type your username"></input>
+        <input type="password" placeholder="Type your password"></input>
+      </form>
+      {/*Signup button links to: /signup*/}
+      <Link to="/signup" className="signup">
+        Signup
+      </Link>
+    </div>
+  );
+};
+
+export default Login;
 //UNIT 3 PERSON
 //authentication POST request
