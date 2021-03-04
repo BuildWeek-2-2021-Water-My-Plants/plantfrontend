7e1fa841df43f09e6d0744c35347e6f7f46e2648
Liza Pincsak <68169418+lizapincsak@users.noreply.github.com>
2021-03-04 13:07:50 -0600

    Revert "plant put working"

--- my-app/src/components/PlantList.js
+++ my-app/src/components/PlantList.js
@@ -1,15 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import { axiosWithAuth } from "../utils/axiosWithAuth";
-
 import PlantCard from "./PlantCard";
-import PlantArray from './PlantArray';
-// import FakePlantCard from './FakePlantCard';
-// import bgSvg from "../images/BGFoliage.svg";
+import FakePlantCard from './FakePlantCard';
+
+// import { connect } from "react-redux";
+// import {fetchUser} from '../actions/index';
+import bgSvg from "../images/BGFoliage.svg";
 
 const PlantList = (props) => {
-  const [plantInfo, setPlantInfo] = useState({});
-  const [plantList, setPlantList] = useState([])
+  // const { isLoading } = props;
+const [plantInfo, setPlantInfo] = useState({});
   const history = useHistory()
 
   const logout = () => {
@@ -26,6 +27,10 @@ const PlantList = (props) => {
     history.push('/addPlant')
   }
 
+  const editPlant = () => {
+    history.push('/updatePlant')
+  }
+
   const editUser = () => {
     history.push('/updateUser')
   }
@@ -39,56 +44,29 @@ const PlantList = (props) => {
       })
       .catch((err) => {
         console.log(err)
+        // debugger;
       })
   }, []);
 
-  useEffect(() => {
-    axiosWithAuth()
-    .get("/users/plants")
-    .then((res) => {
-      console.log(res)
-      setPlantList(res.data)
-    })
-    .catch((err) => {
-      console.log(err)
-    })
-}, []);
-
   return (
+    <>
     <div>
-      <div>
-        <h1>Hi, {plantInfo.username}!</h1>
-        <h3>This is your email: {plantInfo.primaryemail}</h3>
-        <br/>
-      </div>
-      <ul>
-      {/* {plantInfo.plants.length > 0 ?
-          plantInfo.plants.map((plant) => {
-            return <PlantCard 
-            key={plant.plantid} 
-            nickname={plant.nickname} 
-            species={plant.species}/>
-      }) : null} */}
-       {/* {plantInfo.plants.length > 0 ?
-          plantInfo.plants.map((plant) => {
-            return <PlantCard 
-            key={plant.plantid} 
-            nickname={plant.nickname} 
-            species={plant.species}/>
-      }) : null} */}
-      </ul>
-      <PlantArray plantsArray={plantList} updatePlants={setPlantList}/>
-      <div>
-        <button onClick={logout}>Logout</button>
-        <button onClick={addPlant}>Add New Plant</button>
-        <button onClick={editUser}>Edit User</button>
-      </div>
+      <h1>Hi, {plantInfo.username}!</h1>
+      <h3>This is your email: {plantInfo.primaryemail}</h3>
+      <br/>
     </div>
+    {plantInfo.length > 0 ?
+        plantInfo.plants.map((plant) => {
+          return <PlantCard setDeletePlant={setPlantInfo} deletePlant={plantInfo} key={plant.plantid} nickname={plant.nickname} species={plant.species}/>
+    }) : <FakePlantCard />}
+    <div>
+      <button onClick={logout}>Logout</button>
+      <button onClick={addPlant}>Add New Plant</button>
+      <button onClick={editPlant}>Edit Plant</button>
+      <button onClick={editUser}>Edit User</button>
+    </div>
+    </>
   );
 };
 
 export default PlantList;
-
-// import { connect } from "react-redux";
-// import {fetchUser} from '../actions/index';
-/*  : <FakePlantCard /> */
